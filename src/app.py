32db47fa64933a3e734ec14f80783533bb9042ac
Paul Glavich <paulglavich@microsoft.com>
2025-06-30 02:55:29 +0000

    added basic openai sdk usage

--- src/app.py
+++ src/app.py
@@ -1,10 +1,21 @@
 from dotenv import load_dotenv
 import os
 from load_env import load_env
+from openai import OpenAI
 
 def main():
     # Load environment variables from .env files
     load_env()
+
+
+    client = OpenAI()
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input="Write a one-sentence bedtime story about a unicorn."
+    )
+
+    print(response.output_text)
 
 if __name__ == "__main__":
     main()
