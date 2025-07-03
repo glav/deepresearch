f98d6760929d807d2956481f64bb05682104dfaf
Paul Glavich <paulglavich@microsoft.com>
2025-07-03 07:20:06 +0000

    Added AIFoundry 03 deep research example code

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 from basic_client import do_basic_completion
 from openai_deep_client import do_openai_research
 from azure_deep_client import do_azure_research
+from aifoundry_deep_client import do_aifoundry_research
 
 def main():
     # Load environment variables from .env files
@@ -15,6 +16,7 @@ def main():
 
     do_openai_research()
     #do_azure_research()
+    #do_aifoundry_research()
 
 
 if __name__ == "__main__":
